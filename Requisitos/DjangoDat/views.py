a058b79e3e800f34fa440bcf3efb54661c6f1675
mylena-DEV <mylenalo403@gmail.com>
2024-08-29 22:28:07 -0500

    agregar_rutas_url

--- Requisitos/DjangoDat/views.py
+++ Requisitos/DjangoDat/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import *
+from .serializers import DatosSerializer
 
+class DatosViewSet (viewsets.ModelViewSet):
+    queryset = Datos.objects.all()
+    serializer_class = DatosSerializer
 # Create your views here.
